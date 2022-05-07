49d9a6038c918e37efdfb694e03807d93c61e14d
Andrew Kim <andrewkim1@mail.weber.edu>
2022-05-06 23:56:17 -0600

    added profile photo and a header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header'
+import selfie from './sunrise_at_delicate_arch.jpeg';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+
       <header className="App-header">
+          <img src={selfie} alt = "Image of me at Delicate Arch at sunrise" width = "518" height = "533" />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+
+        <div className='header__center'>
+            Andrew Kim Header
+        </div>
+
+    </div>
+  )
+}
+
+export default Header
