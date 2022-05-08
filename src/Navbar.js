81611c968c9022b1e35a86fb655150746ee0dd6a
Andrew Kim <andrewkim1@mail.weber.edu>
2022-05-07 21:01:55 -0600

    added white box to left of navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,7 @@ function Navbar() {
     <div className='navbar'>
       <div className='navbar__links'>
         <div className='navbar__logo'>
+          <img src="https://upload.wikimedia.org/wikipedia/commons/4/45/White_box_55x90.png" alt="logo" />
         </div>
         <div className='navbar__links-container'>
           <p><a href="home">Home</a></p>
