d7423bace23f0d372b26771125d0d6ffda8e3947
jae <50612359+andrewjaekim@users.noreply.github.com>
2022-09-09 19:22:06 -0600

    Update Body.js

--- src/Body.js
+++ src/Body.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Body.css'
 import selfie from './sunrise_at_delicate_arch.jpeg';
+import logo from './tree.jpg';
+
 
 function Body() {
   return (
     <div className='body'>
         <img src={selfie} alt = "Image of me at Delicate Arch at sunrise" width = "518" height = "533" />
+        <img src={logo} alt = "image for espn team lol" />
     </div>
   )
 }
